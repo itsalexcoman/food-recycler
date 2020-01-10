de788672bbc037e11a9b77f09c6b9e19f4ff1b80
Razvan Constantinescu <crazvan97@gmail.com>
2020-01-10 09:53:16 +0000

    Updated createdb.js

--- createdb.js
+++ createdb.js
@@ -2,7 +2,7 @@ var db = require('./backend/models/index');
 
 db.sequelize.sync({force:true}).then(async () => {
     let user = await db.Users.create({
-        id: 1,
+        //id: 1,
         email: "andrei@yahoo.com",
         password: "1234",
         full_name: "Andrei Popescu",
@@ -11,25 +11,25 @@ db.sequelize.sync({force:true}).then(async () => {
     })
 
     let building = await db.Buildings.create({
-        code: 1,
+       // code: 1,
         name: "Cladirea 1"
     })
     
     let group = await db.Groups.create({
-        id: 1,
+       // id: 1,
         name: "Grupul 1",
         description: "Grupul meu de prieteni",
         admin_id: 1
     })
 
     let groupMembership = await db.GroupMembership.create({
-        id: 1,
+       // id: 1,
         user_id: user.id,
         group_id: group.id
     })
     
     let product = await db.Products.create({
-        id:1,
+      //  id:1,
         name: "Lapte Zuzu",
         type: "Lactate",
         user_id: 1,
@@ -37,7 +37,7 @@ db.sequelize.sync({force:true}).then(async () => {
     })
     
      let user2 = await db.Users.create({
-        id: 2,
+       // id: 2,
         email: "andrei2@yahoo.com",
         password: "1234",
         full_name: "Andrei Ionescu",
@@ -46,19 +46,19 @@ db.sequelize.sync({force:true}).then(async () => {
     })
 
     let building2 = await db.Buildings.create({
-        code: 2,
+      //  code: 2,
         name: "Cladirea 2"
     })
     
 
     let groupMembership2 = await db.GroupMembership.create({
-        id: 2,
+      //  id: 2,
         user_id: user2.id,
         group_id: group.id
     })
     
     let product2 = await db.Products.create({
-        id:2,
+     //   id:2,
         name: "Pate Bucegi",
         type: "Conserve",
         user_id: 2,
