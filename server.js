918a7c74d722a953a2ee47c45d96be35714fcd31
Alex Coman <itsalexcoman@gmail.com>
2020-01-10 16:01:18 +0000

    GENERAL: Updated server.js for POST methods

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const api = require('./backend/routes/api')
 const port = 3001
 
-app.get('/', (req, res) => res.send('Welcome to Food Recycler!'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/api', api);
 
+app.get('/', (req, res) => res.send('Welcome to Food Recycler!'))
 app.listen(port, () => console.log(`Food Recycler started on port ${port}\n`))
