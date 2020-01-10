bc5da42301efa5647e2590e138d1424a969477ec
Alex Coman <itsalexcoman@gmail.com>
2020-01-10 13:35:21 +0000

    Changed Membership table name in model

--- backend/models/GroupMembership.js
+++ backend/models/GroupMembership.js
@@ -23,6 +23,8 @@ module.exports = function(sequelize, DataTypes) {
             },
             allowNull: false
         }
+    }, {
+        tableName: "membership"
     });
     return GroupMembership;
 }
