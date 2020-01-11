fe7870f8a3b995cc8f5c9fd9850af06facaeb997
Alex Coman <itsalexcoman@gmail.com>
2020-01-11 09:39:52 +0000

    Updated server.js & start script

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const api = require('./backend/routes/api')
 const port = 3001
+
+const colors = require('colors')
+const cors = require('cors')
+const api = require('./backend/routes/api')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use('/', express.static('frontend/build'))
 app.use('/api', api);
 
-app.get('/', (req, res) => res.send('Welcome to Food Recycler!'))
-app.listen(port, () => console.log(`Food Recycler started on port ${port}\n`))
+app.listen(port, () => console.log(`Food Recycler started on port ${port}\n`.yellow))
