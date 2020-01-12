8ccd67be5deeeaee7f1f0c7723a464bc0ec31f62
Razvan Constantinescu <crazvan97@gmail.com>
2020-01-12 16:35:15 +0000

    Updated AddProduct.js

--- frontend/src/components/forms/AddProduct.js
+++ frontend/src/components/forms/AddProduct.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add'
 import TextField from '@material-ui/core/TextField';
@@ -7,6 +8,21 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export default function AddProduct() {
     const [open, setOpen] = React.useState(false);
@@ -18,6 +34,17 @@ export default function AddProduct() {
     const handleClose = () => {
         setOpen(false);
     };
+    
+    const classes = useStyles();
+  const [type, setType] = React.useState('');
+
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+
+
+  const handleChange = event => {
+    setType(event.target.value);
+  };
 
     return (
         <div>
@@ -31,8 +58,30 @@ export default function AddProduct() {
                         autoFocus
                         margin="dense"
                         id="name"
-                        label="Email Address"
-                        type="email"
+                        label="Name"
+                        fullWidth
+                    />
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={type}
+                            onChange={handleChange}
+                        >
+                            <MenuItem>Conserve</MenuItem>
+                            <MenuItem>Carne</MenuItem>
+                            <MenuItem>Fruncte</MenuItem>
+                            <MenuItem>Legume</MenuItem>
+                            <MenuItem>Desert</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="days_left"
+                        label="Days until expire"
+                        type="number"
                         fullWidth
                     />
                 </DialogContent>
