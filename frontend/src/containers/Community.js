32a493ea0820a275c51660d6a3e86a3a71d45192
Razvan Constantinescu <crazvan97@gmail.com>
2020-01-12 13:37:41 +0000

    Updated Community.js

--- frontend/src/containers/Community.js
+++ frontend/src/containers/Community.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-// import GroupList from "../components/lists/GroupList";
+import GroupList from "../components/lists/GroupList";
 
 class Community extends Component {
   render() {
     return (
       <div>
         <h1>I am Community</h1>
+        <GroupList title="Groups You Manage" />
+        <GroupList title="Groups You're In" />
       </div>
     );
   }
 }
 
 export default Community;
-
-// <GroupList title="Groups You Manage" />
-// <GroupList title="Groups You're In" />
