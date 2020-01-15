90bdf46384b1d7afda15debbb81bb0098b1edbed
Alex Coman <itsalexcoman@gmail.com>
2020-01-15 07:53:56 +0200

    VER: Better than the last one (v0.6.0)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import App from './App';
-import store from './store';
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import store from "./store";
+import "./index.css";
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
